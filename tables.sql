3cc95a9ba470c2a013fd2b328bb2791a77201a78
Takaso Nogokui <70639611+Takaso@users.noreply.github.com>
2025-05-21 19:17:35 +0200

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -7,18 +7,54 @@ CREATE TABLE users (
     bio TEXT,
     cv_file VARCHAR(255),
     address VARCHAR(255),
-    phone VARCHAR(50)
+    phone VARCHAR(50),
+    username VARCHAR(255) DEFAULT ''
 );
 
 CREATE TABLE tags (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) UNIQUE NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE user_tags (
-  user_id INT NOT NULL,
-  tag_id INT NOT NULL,
-  PRIMARY KEY(user_id, tag_id),
-  FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
-  FOREIGN KEY(tag_id) REFERENCES tags(id) ON DELETE CASCADE
+    user_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    PRIMARY KEY(user_id, tag_id),
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY(tag_id) REFERENCES tags(id) ON DELETE CASCADE
+);
+
+CREATE TABLE posts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100),
+    content TEXT NOT NULL,
+    user_id INT NOT NULL,
+    creation_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    image VARCHAR(255);
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE post_tags (
+    post_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    PRIMARY KEY (post_id, tag_id),
+    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
+);
+
+CREATE TABLE post_media (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    post_id INT NOT NULL,
+    file_path VARCHAR(255) NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
+);
+
+CREATE TABLE comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    post_id INT NOT NULL,
+    user_id INT NOT NULL,
+    content TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
